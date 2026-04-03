6aedb1b28bef0f6dc74c460384f3e1f7d8a8274e
XenLian78 <xenlian@yahoo.gr>
2026-04-03 16:10:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
 
+const CACHE_NAME = 'gb-checklist-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './assets/images/G&B-LOGO.svg',
+  './assets/images/MISCELA-DORO-LOGO.svg',
+  './assets/images/apple-icon-180x180.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/signature_pad/4.1.7/signature_pad.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
