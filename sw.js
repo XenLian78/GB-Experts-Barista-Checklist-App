12cdc58f831474f6e708b2480e37b006675260a9
XenLian78 <xenlian@yahoo.gr>
2026-04-14 12:04:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const ASSETS = [
   './manifest.json',
   './assets/images/G&B-LOGO.svg',
   './assets/images/MISCELA-DORO-LOGO.svg',
-  './assets/images/apple-icon-180x180.png',
+  './assets/images/icon-180.png',
   'https://cdnjs.cloudflare.com/ajax/libs/signature_pad/4.1.7/signature_pad.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
